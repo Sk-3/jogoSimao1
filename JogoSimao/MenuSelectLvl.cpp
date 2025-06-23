0b5aad0eb5d0f3c1cdc8c211104994f771b6046b
Ramon Vargas <rv1690@gmail.com>
2025-06-23 00:35:46 -0300

    teste Ranking

--- JogoSimao/MenuSelectLvl.cpp
+++ JogoSimao/MenuSelectLvl.cpp
@@ -13,6 +13,6 @@ MenuSelectLvl::~MenuSelectLvl()
 
 void MenuSelectLvl::carregarBotoes() {
 	buttonVector.push_back(new Entidades::Button(sf::Vector2f(50.f, 100.f), "Voltar", Actions::VOLTAR_1_MENU));
-	buttonVector.push_back(new Entidades::Button(sf::Vector2f(200.f, 300.f), "Level 1", Actions::FASE_1));
-	buttonVector.push_back(new Entidades::Button(sf::Vector2f(200.f, 400.f), "Level 2", Actions::FASE_2));
+	buttonVector.push_back(new Entidades::Button(sf::Vector2f(200.f, 300.f), "Fase 1", Actions::FASE_1));
+	buttonVector.push_back(new Entidades::Button(sf::Vector2f(200.f, 400.f), "Fase 2", Actions::FASE_2));
 }
